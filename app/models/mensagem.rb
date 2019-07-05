1ed1809d7df0b4ae7c1be711c0ed2c25f6d31271
Julyemerson <jb.leonizio@gmail.com>
2019-07-05 13:48:23 -0300

    adicionado validacoes e finalizado o BLOG

--- app/models/mensagem.rb
+++ app/models/mensagem.rb
@@ -1,2 +1,8 @@
 class Mensagem < ApplicationRecord
+    #validates (:titulo, :corpo, :autor, :email, presence :true)
+    validates :titulo, :corpo, :autor, :email, presence: true,
+                        length: {:minimum => 5}
+                        
+     
+
 end
